a37836e089415644b27a370eb6f85066d654f791
Murad Faridi <muradfaridi92@gmail.com>
2020-08-16 01:12:05 +0500

    switch router

---
+++ src/components/lifecycle.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Lifecycle extends Component {
+    render() {
+        return(
+            <div>
+                <h3>Lifecycle</h3>
+            </div>
+        )
+    }
+}
+
+export default Lifecycle
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { BrowserRouter, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom'
 
 // COMPONENTS
 import Home from './components/home'
 import Posts from './components/posts'
 import PostItem from './components/post_item.js'
 import Profiles from './components/profiles'
+import Lifecycle from './components/lifecycle'
 
 const App = () => {
     return (
@@ -24,13 +25,17 @@ const App = () => {
                         hash:'#murad',
                         search:'?profile=true'
                     }}>Profiles</NavLink><br />
+                    <NavLink to="/lifecycle">Lifecycle</NavLink>
                     <hr />
                 </header>
-                <Route render={() => <h3>Oops 404</h3>} />
-                <Route path="/" exact component={Home} />
-                <Route path="/posts" component={Posts} />
-                <Route path="/posts/:id" component={PostItem} />
-                <Route path="/profiles" component={Profiles} />
+                <Switch>
+                    <Route path="/posts/:id" component={PostItem} />
+                    <Route path="/profiles" component={Profiles} />
+                    <Route path="/lifecycle" component={Lifecycle} />
+                    <Route path="/posts" component={Posts} />
+                    <Route path="/" exact component={Home} />
+                    <Route render={() => <h3>Oops 404</h3>} />
+                </Switch>
             </div>
         </BrowserRouter>
     )
