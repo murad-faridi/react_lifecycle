207fc32871026b0500e2d3ffb98097f6d37ecd76
Murad Faridi <muradfaridi92@gmail.com>
2020-08-16 01:16:49 +0500

    component lifycycle

--- src/components/lifecycle.js
+++ src/components/lifecycle.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 
 class Lifecycle extends Component {
+    
+    // 1- get default props
+
+    // 2- set default state
+    state = {
+        title: 'Life Cycle'
+    }
+
+    // 3- before render
+
+    // 4- render jsx
     render() {
         return(
             <div>
-                <h3>Lifecycle</h3>
+                <h3>{this.state.title}</h3>
             </div>
         )
     }
+
+    // 5- after render
 }
 
 export default Lifecycle
